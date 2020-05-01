1b018639ea4aeaeedfb6e10e9cf4980a276473cc
JustAnotherDay <senierharold@gmail.com>
2020-05-01 21:17:20 +0800

    2 files modified

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,12 +1,47 @@
-import {Component} from '@angular/core';
-import {VERSION} from '@angular/material';
-
+import {
+  Component,
+  ElementRef,
+  ViewChild,
+  AfterViewInit,
+  HostListener
+} from "@angular/core";
+import { VERSION } from "@angular/material";
+import { MatStepper } from "@angular/material/stepper";
 @Component({
-  selector: 'material-app',
-  templateUrl: 'app.component.html'
+  selector: "material-app",
+  templateUrl: "app.component.html",
+  styles: [".stepzero { background:black; }"]
 })
-export class AppComponent { 
+export class AppComponent implements AfterViewInit {
   version = VERSION;
+
+  @ViewChild("stepper") stepper: MatStepper;
+
+  lastSelected = 0;
+
+  ngAfterViewInit() {
+    const selectedIndex = this.stepper.selectedIndex.toString();
+    //debugger;
+    var _this = this;
+    this.stepper._stepHeader._results.forEach(stepHeader => {
+      stepHeader.nativeElement.addEventListener(
+        "click",
+        this.onStepHeaderClick.bind(
+          null,
+          stepHeader.nativeElement.attributes[4].value,
+          _this,
+          this.stepper
+        )
+      );
+    });
+  }
+
+  onStepHeaderClick = (_clickedIndex, _this, stepper) => {
+    if (_this.lastSelected == _clickedIndex) stepper.selectedIndex = 0
+    else console.log("goto clicked");
+
+    _this.lastSelected = _clickedIndex;
+  };
 }
 
 /**
